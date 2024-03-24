9ae1bf8fadac8d5aa79c4f5292b241c775e8a851
Wajahat <wajahath533@gmail.com>
2024-03-24 18:23:00 +0500

    fyp discussion portal

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from . models import Notes
 
 def home(request):
     return render(request,'dashboard/home.html')
+def notes(request):
+    notes=Notes.objects.filter(user=request.user)
+    context={
+        'notes': notes
+    }
+    return render(request,'dashboard/notes.html',context)
 
 # Create your views here.
