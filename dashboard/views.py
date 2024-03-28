ea991bf174f1c41d7df3ca1996032096bda7ab49
Wajahat <wajahath533@gmail.com>
2024-03-28 18:32:38 +0500

    fyp-discussion-portal

--- dashboard/views.py
+++ dashboard/views.py
@@ -29,6 +29,7 @@ class NotesDetailView(generic.DetailView):
     model = Notes
 
 def homework(request):
+    form=HomeworkForm()
     homework=Homework.objects.filter(user=request.user)
     if len(homework) == 0:
         homework_done = True
@@ -36,7 +37,7 @@ def homework(request):
         homework_done = False
 
     context={
-        'homeworks':homework, 'homework_done':homework_done
+        'homeworks':homework, 'homework_done':homework_done,'form':form
     }
 
     return render(request,'dashboard/homework.html',context)
