1708a766c7756989390fa94b20c9b4e54cffc5ff
Wajahat <wajahath533@gmail.com>
2024-04-12 10:15:51 +0500

    fyp discussion portal

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name="home"),
     path('notes',views.notes, name="notes"),
     path('delete_note/<int:pk>',views.delete_note, name="delete_note"),
     path('notes_detail/<int:pk>',views.NotesDetailView.as_view(), name="notes_detail"),
